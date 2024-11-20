53204da0bce9cc81e48a62924687e787e461ab9c
Katya Polyak <katya_7002@mail.ru>
2024-11-20 21:10:49 +0300

    create __init__.py

---
+++ collision/__init__.py
@@ -0,0 +1,8 @@
+# Импортируем все функции из файла functions.py
+from .functions import (
+    isCorrectRect,
+    isCollisionRect,
+    intersectionAreaRect,
+    intersectionAreaMultiRect,
+    RectCorrectError
+)
